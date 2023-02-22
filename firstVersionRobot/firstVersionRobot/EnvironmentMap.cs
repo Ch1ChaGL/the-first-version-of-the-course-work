2fcda1c949c8124019820cccbd657a8be91da3d8
Danil Markov <102900110+Ch1ChaGL@users.noreply.github.com>
2023-02-22 21:31:56 +0300

    Add files via upload

---
+++ firstVersionRobot/firstVersionRobot/EnvironmentMap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace firstVersionRobot
+{
+    internal class EnvironmentMap
+    {
+        private int _width;
+        private int _height;
+        private DataGridView _dataGridView;
+
+        public EnvironmentMap(int width, int height, DataGridView dataGridView)
+        {
+            _width = width;
+            _height = height;
+            _dataGridView = dataGridView;
+            InitializeDataGridView();
+        }
+        private void InitializeDataGridView()
+        {
+            _dataGridView.Visible = true;
+            _dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            _dataGridView.ScrollBars = ScrollBars.None;
+            _dataGridView.AllowUserToResizeColumns = false;
+            _dataGridView.AllowUserToResizeRows = false;
+            _dataGridView.AutoSize = false;
+            _dataGridView.RowCount = _height;
+            _dataGridView.ColumnCount = _width;
+            _dataGridView.Width = calculateDataGridViewSize(_width);
+            _dataGridView.Height = calculateDataGridViewSize(_height);
+            for (int i = 0; i < _width; i++)
+            {
+                _dataGridView.Columns[i].Width = 50;                          
+            }
+            for (int j = 0; j < _height; j++)
+            {
+                _dataGridView.Rows[j].Height = 50;
+            }
+        }
+        private int calculateDataGridViewSize(int size) {
+            return size * 50;
+        }
+    }
+}
---
+++ firstVersionRobot/firstVersionRobot/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace firstVersionRobot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //DataGridViewImageColumn imageColumn = new DataGridViewImageColumn();
+            //imageColumn.Width = 50;
+            //dataGridView1.Columns.Add(imageColumn);
+
+            //var imageCell = new DataGridViewImageCell();
+            //var image = Properties.Resources.turtle;
+            //imageCell.Value = image;
+            //imageCell.ImageLayout = DataGridViewImageCellLayout.Stretch;
+
+
+            //dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            //dataGridView1.ScrollBars = ScrollBars.None;
+            //dataGridView1.AllowUserToResizeColumns = false;
+            //dataGridView1.AllowUserToResizeRows = false;
+            //dataGridView1.AutoSize = false;
+            //dataGridView1.RowCount = 11;
+            //dataGridView1.ColumnCount = 11;
+            //dataGridView1.Rows[0].Cells[0] = imageCell;
+            ////dataGridView1.Rows[0].Cells[0].Value = image;
+            ////((DataGridViewImageCell)dataGridView1.Rows[0].Cells[0]).ImageLayout = DataGridViewImageCellLayout.Stretch;
+            //dataGridView1.Width = 550;
+            //dataGridView1.Height = 550;
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    dataGridView1.Columns[i].Width = 50;
+            //    dataGridView1.Rows[i].Height = 50;
+            //}
+
+            EnvironmentMap environmentMap = new EnvironmentMap(10,11,dataGridView1);
+           
+        }
+        
+    }
+}
