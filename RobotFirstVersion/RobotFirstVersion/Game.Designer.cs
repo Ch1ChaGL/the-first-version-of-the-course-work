2360338656ab5c68d1e41ca56d6881da0e66d582
Danil Markov <102900110+Ch1ChaGL@users.noreply.github.com>
2023-03-18 12:38:54 +0300

    Add files via upload

--- RobotFirstVersion/RobotFirstVersion/Game.Designer.cs
+++ RobotFirstVersion/RobotFirstVersion/Game.Designer.cs
@@ -37,6 +37,9 @@
             this.Play = new System.Windows.Forms.Button();
             this.NextStep = new System.Windows.Forms.Button();
             this.Reset = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Copy = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,6 +58,7 @@
             this.TextField.Size = new System.Drawing.Size(519, 378);
             this.TextField.TabIndex = 1;
             this.TextField.Text = "";
+            this.TextField.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextField_KeyDown);
             // 
             // Up
             // 
@@ -74,7 +78,7 @@
             this.Down.TabIndex = 3;
             this.Down.Text = "Down";
             this.Down.UseVisualStyleBackColor = true;
-            this.Down.Click += new System.EventHandler(this.Down_Click_1);
+            this.Down.Click += new System.EventHandler(this.Up_Click);
             // 
             // Left
             // 
@@ -84,7 +88,7 @@
             this.Left.TabIndex = 4;
             this.Left.Text = "Left";
             this.Left.UseVisualStyleBackColor = true;
-            this.Left.Click += new System.EventHandler(this.Left_Click);
+            this.Left.Click += new System.EventHandler(this.Up_Click);
             // 
             // Right
             // 
@@ -94,7 +98,7 @@
             this.Right.TabIndex = 5;
             this.Right.Text = "Right";
             this.Right.UseVisualStyleBackColor = true;
-            this.Right.Click += new System.EventHandler(this.Right_Click);
+            this.Right.Click += new System.EventHandler(this.Up_Click);
             // 
             // Play
             // 
@@ -118,19 +122,52 @@
             // 
             // Reset
             // 
-            this.Reset.Location = new System.Drawing.Point(955, 489);
+            this.Reset.Location = new System.Drawing.Point(955, 549);
             this.Reset.Name = "Reset";
             this.Reset.Size = new System.Drawing.Size(103, 42);
             this.Reset.TabIndex = 8;
             this.Reset.Text = "Reset";
             this.Reset.UseVisualStyleBackColor = true;
-            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            this.Reset.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(671, 549);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(103, 42);
+            this.Delete.TabIndex = 9;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(809, 549);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(103, 42);
+            this.Copy.TabIndex = 10;
+            this.Copy.Text = "Copy";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(1087, 549);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(103, 42);
+            this.Clear.TabIndex = 11;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Delete_Click);
             // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1202, 689);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.Delete);
             this.Controls.Add(this.Reset);
             this.Controls.Add(this.NextStep);
             this.Controls.Add(this.Play);
@@ -158,5 +195,8 @@
         private System.Windows.Forms.Button Play;
         private System.Windows.Forms.Button NextStep;
         private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button Clear;
     }
 }
--- RobotFirstVersion/RobotFirstVersion/Game.cs
+++ RobotFirstVersion/RobotFirstVersion/Game.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RobotFirstVersion
 {
@@ -30,25 +31,25 @@ namespace RobotFirstVersion
             robot.moveDown();
         }
 
-        private void Up_Click(object sender, EventArgs e)
-        {
-            TextField.Text += "Вверх\n";
-        }
+        //private void Up_Click(object sender, EventArgs e)
+        //{
+        //    TextField.Text += "Вверх\n";
+        //}
 
-        private void Down_Click_1(object sender, EventArgs e)
-        {
-            TextField.Text += "Вниз\n";
-        }
+        //private void Down_Click_1(object sender, EventArgs e)
+        //{
+        //    TextField.Text += "Вниз\n";
+        //}
 
-        private void Left_Click(object sender, EventArgs e)
-        {
-            TextField.Text += "Влево\n";
-        }
+        //private void Left_Click(object sender, EventArgs e)
+        //{
+        //    TextField.Text += "Влево\n";
+        //}
 
-        private void Right_Click(object sender, EventArgs e)
-        {
-            TextField.Text += "Вправо\n";
-        }
+        //private void Right_Click(object sender, EventArgs e)
+        //{
+        //    TextField.Text += "Вправо\n";
+        //}
 
         private void Play_Click(object sender, EventArgs e)
         {   parser = new ParserCommand(TextField.Text);
@@ -90,9 +91,153 @@ namespace RobotFirstVersion
 
         private void Reset_Click(object sender, EventArgs e)
         {
+            
+        }
+
+        private void TextField_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.SuppressKeyPress = true;
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                // Вставляем текст из буфера обмена
+                TextField.Paste();
+
+            }
+            if (TextField.Text.Length > 0) {
+                if (e.Control && e.KeyCode == Keys.C)
+                {
+                    Clipboard.SetText(TextField.SelectedText);
+                    e.Handled = true;
+                }              
+                else if (e.KeyCode == Keys.Back || e.KeyCode == Keys.Delete)
+                {
+                    FnDelete();
+                    e.Handled = true;
+                }
+
+
+
+            }          
+        }
+
+        private void Up_Click(object sender, EventArgs e)
+        {
+            string direction = "";
+            var button = sender as Button;
+            if (button.Name == "Up")
+            {
+                direction = "Вверх\n";
+            }
+            if (button.Name == "Left")
+            {
+                direction = "Влево\n";
+            }
+            if (button.Name == "Down")
+            {
+                direction = "Вниз\n";
+            }
+            if (button.Name == "Right")
+            {
+                direction = "Вправо\n";
+            }
+
+            // Получаем номер строки, на которой находится курсор
+            int currentLineIndex = TextField.GetLineFromCharIndex(TextField.SelectionStart);
+
+            // Находим начало следующей строки
+            int nextLineStart = TextField.GetFirstCharIndexFromLine(currentLineIndex + 1);
+            if (nextLineStart == -1)
+            {
+                // Если следующей строки нет, то добавляем слово в конец текста
+                nextLineStart = TextField.Text.Length;              
+            }
+           
+
+            // Добавляем слово на позицию nextLineStart
+            TextField.Text = TextField.Text.Insert(nextLineStart, direction);
+
+            // Перемещаем курсор в конец добавленного слова
+            TextField.SelectionStart = nextLineStart + direction.Length;
+
+            // Перемещаем фокус на текстовое поле
+            TextField.Focus();
+        }
+
+
+
+
+        private void FnDelete()
+        {
+            int currentLineIndex = TextField.GetLineFromCharIndex(TextField.SelectionStart);
+            int currentLineStart = TextField.GetFirstCharIndexFromLine(currentLineIndex);
+            int currentLineEnd = TextField.GetFirstCharIndexFromLine(currentLineIndex + 1);
+            if (currentLineEnd == -1)
+            {
+                //currentLineEnd = TextField.Text.Length;
+                currentLineEnd = currentLineStart;
+                currentLineStart = TextField.GetFirstCharIndexFromLine(currentLineIndex - 1);
+
+                if (currentLineStart == -1)
+                {
+                    currentLineStart = 0;
+                }
+            }
+
+
+
+            // Сохраняем текущую позицию курсора
+            int cursorPosition = TextField.SelectionStart;
+
+
+
+            TextField.Text = TextField.Text.Remove(currentLineStart, currentLineEnd - currentLineStart);
+
+
+            // Восстанавливаем позицию курсора
+            if (cursorPosition >= currentLineStart)
+            {
+                if (cursorPosition <= currentLineEnd)
+                {
+                    TextField.SelectionStart = currentLineStart;
+                }
+                else
+                {
+                    TextField.SelectionStart = cursorPosition - (currentLineEnd - currentLineStart);
+                }
+            }
+        }
+        private void FnClear() { TextField.Clear(); }
+        private void FnCopy()
+        {
+            Clipboard.SetText(TextField.Text);
+        }
+        private void FnReset()
+        {
             TextField.Text = "";
             maze.resetMap();
             parser = null;
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            if(button.Name == "Delete")
+            {
+                FnDelete();
+            }
+            if (button.Name == "Clear")
+            {
+                FnClear();
+            }
+            if (button.Name == "Reset")
+            {
+                FnReset();
+            }
+            if (button.Name == "Copy")
+            {
+                FnCopy();
+            }
         }
     }
 }
